524bd16ea0529834d8670f8f528f63c9274a687c
Luiso9 <muhandreop@gmail.com>
2025-06-16 20:13:48 +0700

    fix: path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
@@ -17,4 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/driannsa/',
 })
