06eac7f601cb346266dcd803e216c7b2dd49637a
Luiso9 <muhandreop@gmail.com>
2025-06-16 20:19:16 +0700

    ci: attempt fix for deployment MIME issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/driannsa/',
   plugins: [
     vue(),
     vueDevTools(),
